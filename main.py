aa77d2adedd16b7156073b3548cbd0dfdb853ff7
AJ Keller <aj@neurosity.co>
2020-02-13 15:14:41 -0500

    :rocket:

---
+++ main.py
@@ -0,0 +1,19 @@
+"""Example program to show how to read a multi-channel time series from LSL."""
+
+from pylsl import StreamInlet, resolve_stream
+
+try:
+    # first resolve an EEG stream on the lab network
+    print("looking for an EEG stream...")
+    streams = resolve_stream('type', 'EEG')
+
+    # create a new inlet to read from the stream
+    inlet = StreamInlet(streams[0])
+    while True:
+        # get a new sample (you can also omit the timestamp part if you're not
+        # interested in it)
+        sample, timestamp = inlet.pull_sample()
+        print(timestamp, sample)
+except KeyboardInterrupt as e:
+    print("Ending program")
+    raise e
